a07c016694aea613e32c076e13b82b9af8387878
unifiedtechsolutions1234-ux <unifiedtechsolutions1234@gmail.com>
2025-10-28 19:39:38 +0500

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -10,7 +10,7 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(cors({ origin: "http://localhost:5173" }));
+app.use(cors({ origin: "*" }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
